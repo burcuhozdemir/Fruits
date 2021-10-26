e438675c672f48f59ab440a052305a22dee0c3c2
Computer <>
2021-10-26 15:59:30 +0300

    first commit

---
+++ Fruits/View/SettingsLabelView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsLabelView.swift
+//  Fruits
+//
+//  Created by Computer on 26.10.2021.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView()
+    }
+}
---
+++ Fruits/View/SettingsRowView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsRowView.swift
+//  Fruits
+//
+//  Created by Computer on 26.10.2021.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsRowView()
+    }
+}
